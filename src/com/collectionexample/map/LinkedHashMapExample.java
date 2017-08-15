2bb504d8ef81e208a1be0af5c37dcf9420c9ba80
Hemantapaudel <hemantapaudel05@gmail.com>
2017-08-16 00:00:14 +0530

    adding linked hashmap

--- src/com/collectionexample/map/LinkedHashMapExample.java
+++ src/com/collectionexample/map/LinkedHashMapExample.java
@@ -1,54 +1,36 @@
 package com.collectionexample.map;
 
 import java.util.LinkedHashMap;
-import java.util.Map.Entry;
+import java.util.Map;
 
-class LRUCache<Integer, String> extends LinkedHashMap<Integer, String>{
-	
-	private static final long serialVersionUID = 1L;
-	
-	static int count =100;
-	public LRUCache(){
-		super(16,0.75f,true);
-	}
-	
-	
-	@Override
-	protected boolean removeEldestEntry(
-			java.util.Map.Entry<Integer, String> eldest) {
-		System.out.println("======count==="+count + "key = "+ eldest.getKey());
-		 //return size()>10?true:false;
-		 return true;
-	}
-	
-}
-
-
+/**
+ *  LinkedHashMap maintain the insertion order and also
+ *  maintained the size of map if required
+ *  
+ *  it will remove eldest entry first.
+ *
+ */
 public class LinkedHashMapExample {
-	
-	
 	
 
 	public static void main(String[] args) {
-		LinkedHashMap<Integer,String> linkedHashMap=new LRUCache<Integer,String>();
+		
+		Map<Integer,String> linkedHashMap=new LinkedHashMap<Integer,String>(){
+			private static final long serialVersionUID = -7822062564231729253L;
+			
+			protected boolean removeEldestEntry(java.util.Map.Entry<Integer,String> eldest) {
+				System.out.println(eldest.getValue());
+				return this.size()>2;
+			}
+		};
 		  
 		linkedHashMap.put(101,"Amit");  
 		linkedHashMap.put(102,"Vijay");  
 		linkedHashMap.put(103,"Rahul");  
+		linkedHashMap.put(104,"Rahul1");  
 		System.out.println(linkedHashMap.size());
 		
-	   /* int i=104;
-	    for( ;i<200;i++){
-	    	System.out.println("======i==="+i);
-	    	linkedHashMap.put(i, "amit");
-	    }*/
-	    
-	   
-	    
-		/*//LinkedHashIterator is internally used for iterate the for loop
-		for(Entry<Integer,String> m:linkedHashMap.entrySet()){  
-			   System.out.println(m.getKey()+" "+m.getValue()); 
-		}*/
+	  
 				  	  
 	}
 
